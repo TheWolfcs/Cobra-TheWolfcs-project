0658b7370054917f107895969fe23f1666f5c075
TheWolfcs <148289342+TheWolfcs@users.noreply.github.com>
2023-10-20 13:43:59 +0100

    Add files via upload

--- encryption.sh
+++ encryption.sh
@@ -9,19 +9,21 @@ echo "
 "
 encrypt_text() {
     local input_text="$1"
+    local key="$2"
     local encrypted_text=""
     
     for ((i = 0; i < ${#input_text}; i++)); do
         char="${input_text:$i:1}"
-        ascii_value=$(printf "%d" "'$char")
-	encrypted_ascii_value=$((ascii_value - 2))
+        av=$(printf "%d" "'$char")
+	encrypted_ascii_value=$((av - key))
         encrypted_char=$(printf \\x$(printf %x "$encrypted_ascii_value"))
         encrypted_text="${encrypted_text}${encrypted_char}"
     done
     
     echo "$encrypted_text"
 }
+read -p "Enter the key:" key 
 read -p "Enter the text to encrypt: " input_text
-encrypted_text=$(encrypt_text "$input_text")
+encrypted_text=$(encrypt_text "$input_text" "$key")
 
 echo "Encrypted text: $encrypted_text"
